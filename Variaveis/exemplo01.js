d6e60c3e33037fcca3311e103b0db64c02a0ca8a
João Refinetti <54112729+Penguin1411@users.noreply.github.com>
2021-10-26 09:37:33 -0300

    Add files via upload

---
+++ Variaveis/exemplo01.js
@@ -0,0 +1,9 @@
+var texto = "Vamos fazer uma calculadora";
+var num1 = 25;
+var num2 = 26;
+soma = num1+num2
+
+    document.getElementById('teste').innerHTML = (texto);
+    alert(soma);
+    //document.write(texto)
+    console.log(num1);
